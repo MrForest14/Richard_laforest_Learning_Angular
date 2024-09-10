7d17a339b51e4902ae63914d5ebb5214b88acb63
Richie <w0693524@myscc.ca>
2024-09-10 11:03:04 -0400

    “Created starter Angular file and completed Assignment 1”

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Richie-laforest-learning-Angular';
+  title = 'Richie laforest';
+  age = '26';
 }
